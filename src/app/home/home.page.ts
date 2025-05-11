b1b0eada8d432f32bccca5b59a8f2f444bffc8dc
Francis0023 <150805817+Francis0023@users.noreply.github.com>
2025-05-10 20:49:04 -0500

    Formulario de creacion de Canal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { IonicModule, AlertController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  constructor() {}
+// Componente principal de la aplicacion 
+export class HomePage implements OnInit {
+  // DEfinicion de las variables 
+  formulario!: FormGroup;
+  // Variables del Formulario 
+  constructor(
+    private fb: FormBuilder,
+    private firestore: Firestore,
+    private alertController: AlertController
+  ) {}
+  // Inicializacion del formlario para la creacion de un Canal 
+  ngOnInit() {
+    this.formulario = this.fb.group({
+      nombre: ['', Validators.required],
+      descripcion: [''],
+      categoria: ['', Validators.required],
+      fechaCreacion: ['', Validators.required],
+      pais: [''],
+      correo: ['', [Validators.required, Validators.email]],
+      monetizado: [false],
+      redes: [[]]
+    });
+  }
+  // Accion para guardar los datos en Firebase
+  async guardar() {
+    const datos = this.formulario.value;
+    const ref = collection(this.firestore, 'canales');
+    try {
+      await addDoc(ref, datos);
+      this.formulario.reset();
+      this.presentAlert('Canal guardado correctamente');
+    } catch (error) {
+      console.error(error);
+      this.presentAlert('Error al guardar los datos');
+    }
+  }
+// Funcion para mostrar un mensaje de alerta de la verificacion de los datos guardados 
+  async presentAlert(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'Mensaje',
+      message: mensaje,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,23 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment';
+import { provideHttpClient } from '@angular/common/http';
 
+// Configuracion de Firebase para la aplicacion y la inicilizacion de la aplicacion
 bootstrapApplication(AppComponent, {
   providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    // Designacion del proveedor de Firebase
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    // Proveedor de rutas
     provideRouter(routes, withPreloading(PreloadAllModules)),
-  ],
+    // Proveedor de Ionic
+    provideIonicAngular(),
+    // Estrategia de reuso de rutas
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ]
 });
